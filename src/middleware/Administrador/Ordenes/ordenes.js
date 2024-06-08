22394e12c065e70d1adf11a690efcabda7f5df11
Cliff-Vic <cliffvicunaguerrero@gmail.com>
2024-06-08 16:38:11 -0500

    Culminacion de la configuracion para que funciones la vista Administrador Ordenes Ordenes

--- src/middleware/Administrador/Ordenes/ordenes.js
+++ src/middleware/Administrador/Ordenes/ordenes.js
@@ -3,14 +3,20 @@ import modelUsuario from '../../../models/usuario.js'
 import modelPersona from '../../../models/persona.js'
 // Orden
 import modelOrden from '../../../models/orden.js'
+import modelOrdEstado from '../../../models/ordenEstado.js'
 
 let usuarios = [...modelUsuario];
 let personas = [...modelPersona];
 let ordenes = [...modelOrden];
+let ordenEstado = [...modelOrdEstado];
 
 const findAll = () => {
     const result = ordenes
         .map(orden => {
+            //Para el reconocimiento del estado
+            let ordEstado = ordenEstado.find(ordEstado => ordEstado.id === orden.estado_id);
+            const estado = ordEstado ? ordEstado.nombre : "Estado desconocido";
+            // Para identifiacion del usuario
             let usuario = usuarios.find(usuario => usuario.id === orden.usuario_id);
             if (usuario !== undefined) {
                 let persona = personas.find(persona => persona.id === usuario.persona_id);
@@ -21,7 +27,8 @@ const findAll = () => {
                         usuario: `${persona.nombre}, ${persona.apellido}`,
                         correo: usuario.correo,
                         fechaOrden: orden.fecha,
-                        estado: orden.estado,
+                        estado_id: orden.estado_id,
+                        estado: estado,
                         total: orden.total
                     };
                 }
